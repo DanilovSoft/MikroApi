4b25112d5071022ccc2e187a3704f7766addaeff
Vitaliy Danilov <vitaliy931@gmail.com>
2019-12-06 11:56:40 +0200

    ThreadLooper upd

--- MikroApi/MikroTikSocket.cs
+++ MikroApi/MikroTikSocket.cs
@@ -8,7 +8,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Danilovsoft.ThreadLooper;
+using DanilovSoft.ThreadLooper;
 
 namespace DanilovSoft.MikroApi
 {
